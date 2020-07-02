f7514ce7fda27ebf767cf03923605358086946b7
bomorov <bomorov199@gmail.com>
2020-07-02 18:22:03 +0600

    Вроде бы всё готово :)

--- MariaFuncs/Form1.vb
+++ MariaFuncs/Form1.vb
@@ -1,9 +1,7 @@
 ﻿Public Class Form1
     Private Const A = 2, B = 2, C = 3
 
-    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView1.SelectedIndexChanged
 
-    End Sub
 
     'Функция-обработчик нажатия клавиши Enter
     Protected Overrides Function ProcessCmdKey(ByRef msg As Message, ByVal keyData As Keys) _
@@ -30,10 +28,22 @@
             Dim answer13 As Double = 3.85 * x * x * Math.Log10(Math.Abs(A - Math.Pow(B, x + C)))
             Dim answer14 As Double = Math.Pow(x, 4) / (1 + (Math.Pow(x, B) / (1 + Math.Pow(x, C))))
             Dim ans1 As String = answer2.ToString()
-            ListView1.TileSize = New Size(ans1.Length * 8, 20)
-            ListView1.Items.Add(ans1)
 
-            txtAnswers.Text = answer1.ToString + Environment.NewLine
+
+            txtAnswers.Text = "Equatation 1: " + answer1.ToString + Environment.NewLine
+            txtAnswers.Text += "Equatation 2: " + answer2.ToString + Environment.NewLine
+            txtAnswers.Text += "Equatation 3: " + answer3.ToString + Environment.NewLine
+            txtAnswers.Text += "Equatation 4: " + answer4.ToString + Environment.NewLine
+            txtAnswers.Text += "Equatation 5: " + answer5.ToString + Environment.NewLine
+            txtAnswers.Text += "Equatation 6: " + answer6.ToString + Environment.NewLine
+            txtAnswers.Text += "Equatation 7: " + answer7.ToString + Environment.NewLine
+            txtAnswers.Text += "Equatation 8: " + answer8.ToString + Environment.NewLine
+            txtAnswers.Text += "Equatation 9: " + answer9.ToString + Environment.NewLine
+            txtAnswers.Text += "Equatation 10: " + answer10.ToString + Environment.NewLine
+            txtAnswers.Text += "Equatation 11: " + answer11.ToString + Environment.NewLine
+            txtAnswers.Text += "Equatation 12: " + answer12.ToString + Environment.NewLine
+            txtAnswers.Text += "Equatation 13: " + answer13.ToString + Environment.NewLine
+            txtAnswers.Text += "Equatation 14: " + answer14.ToString + Environment.NewLine
             Return True
         End If
         Return MyBase.ProcessCmdKey(msg, keyData)
