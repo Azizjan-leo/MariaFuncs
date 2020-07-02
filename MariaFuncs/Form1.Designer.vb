9ea91368e2bda8a31df0affb2da15ed426fc78ad
Azizjan Ayupov <litpulla@mail.ru>
2020-07-02 15:13:48 +0600

    Frame

--- MariaFuncs/Form1.Designer.vb
+++ MariaFuncs/Form1.Designer.vb
@@ -22,17 +22,89 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.txtX = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblZ = New System.Windows.Forms.Label()
+        Me.txtZ = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
+        '
+        'txtX
+        '
+        Me.txtX.Location = New System.Drawing.Point(66, 42)
+        Me.txtX.Name = "txtX"
+        Me.txtX.Size = New System.Drawing.Size(60, 20)
+        Me.txtX.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Swis721 Hv BT", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(37, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(23, 18)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "X:"
+        '
+        'ListView1
+        '
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(12, 68)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(282, 267)
+        Me.ListView1.TabIndex = 3
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Swis721 Hv BT", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(23, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(259, 18)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Please type values and pres Enter"
+        '
+        'lblZ
+        '
+        Me.lblZ.AutoSize = True
+        Me.lblZ.Font = New System.Drawing.Font("Swis721 Hv BT", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblZ.Location = New System.Drawing.Point(154, 44)
+        Me.lblZ.Name = "lblZ"
+        Me.lblZ.Size = New System.Drawing.Size(23, 18)
+        Me.lblZ.TabIndex = 6
+        Me.lblZ.Text = "Z:"
+        '
+        'txtZ
+        '
+        Me.txtZ.Location = New System.Drawing.Point(183, 42)
+        Me.txtZ.Name = "txtZ"
+        Me.txtZ.Size = New System.Drawing.Size(60, 20)
+        Me.txtZ.TabIndex = 5
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(306, 347)
+        Me.Controls.Add(Me.lblZ)
+        Me.Controls.Add(Me.txtZ)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtX)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Maria Funcs"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents txtX As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblZ As Label
+    Friend WithEvents txtZ As TextBox
 End Class
--- MariaFuncs/Form1.vb
+++ MariaFuncs/Form1.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Form1
+    Private Const A = 1, B = 2, C = 3
+
+    'Функция-обработчик нажатия клавиши Enter
+    Protected Overrides Function ProcessCmdKey(ByRef msg As Message,
+                                           ByVal keyData As Keys) _
+                                           As Boolean
+
+        If msg.WParam.ToInt32() = CInt(Keys.Enter) Then
+            Dim x As Double = txtX.Text
+            Dim z As Double = txtZ.Text
+
+            'ToDo Write your code here
+
+            Return True
+        End If
+        Return MyBase.ProcessCmdKey(msg, keyData)
+    End Function
+
 
 End Class
