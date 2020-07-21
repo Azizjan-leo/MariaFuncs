b4ad3772ff9f39e0ef9f6ad3471cb983c9a4ca98
bomorov <bomorov199@gmail.com>
2020-07-22 03:15:18 +0600

    Answer were removed

--- MariaFuncs/Form1.vb
+++ MariaFuncs/Form1.vb
@@ -1,6 +1,10 @@
 ﻿Public Class Form1
     Private Const A = 2, B = 2, C = 3
 
+    Private Sub txtAnswers_TextChanged(sender As Object, e As EventArgs) Handles txtAnswers.TextChanged
+
+    End Sub
+
 
 
     'Функция-обработчик нажатия клавиши Enter
@@ -30,20 +34,7 @@
             Dim ans1 As String = answer2.ToString()
 
 
-            txtAnswers.Text = "Equatation 1: " + answer1.ToString + Environment.NewLine
-            txtAnswers.Text += "Equatation 2: " + answer2.ToString + Environment.NewLine
-            txtAnswers.Text += "Equatation 3: " + answer3.ToString + Environment.NewLine
-            txtAnswers.Text += "Equatation 4: " + answer4.ToString + Environment.NewLine
-            txtAnswers.Text += "Equatation 5: " + answer5.ToString + Environment.NewLine
-            txtAnswers.Text += "Equatation 6: " + answer6.ToString + Environment.NewLine
-            txtAnswers.Text += "Equatation 7: " + answer7.ToString + Environment.NewLine
-            txtAnswers.Text += "Equatation 8: " + answer8.ToString + Environment.NewLine
-            txtAnswers.Text += "Equatation 9: " + answer9.ToString + Environment.NewLine
-            txtAnswers.Text += "Equatation 10: " + answer10.ToString + Environment.NewLine
-            txtAnswers.Text += "Equatation 11: " + answer11.ToString + Environment.NewLine
-            txtAnswers.Text += "Equatation 12: " + answer12.ToString + Environment.NewLine
-            txtAnswers.Text += "Equatation 13: " + answer13.ToString + Environment.NewLine
-            txtAnswers.Text += "Equatation 14: " + answer14.ToString + Environment.NewLine
+
             Return True
         End If
         Return MyBase.ProcessCmdKey(msg, keyData)
